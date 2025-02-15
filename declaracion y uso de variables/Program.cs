60a3caffacaa9ad1c7325c9dbfc405789b88fc1d
Jose Rosales <Jrosalesp16@miumg.edu.gt>
2025-02-14 22:32:59 -0600

    Agregado proyecto declaracion y uso de variables

---
+++ declaracion y uso de variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int numeroEntero;
+        double numeroDecimal;
+        string texto;
+        bool valorBooleano;
+
+        Console.WriteLine("Ingresa un numero entero: ");
+        numeroEntero = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa un numero decimal: ");
+        numeroDecimal = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese un texto: ");
+        texto = Console.ReadLine();
+
+        Console.Write("Ingrese un valor veradero o falso (true/false): ");
+        valorBooleano = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine("El numero entero es: " + numeroEntero);
+        Console.WriteLine("El numero decimal es: " + numeroDecimal);
+        Console.WriteLine("El texto es: " + texto);
+        Console.WriteLine("el valor booleano es: " + valorBooleano);
+    }
+}
